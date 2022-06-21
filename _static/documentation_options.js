5bff25f5dd40981378b66213ec6f5edba6eaf138
yut23 <yut23@users.noreply.github.com>
2022-06-21 19:09:45 +0000

    deploy: b5686346a89ff16177c12a88d8c4eb723e6e1caa

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '1.6.0.post125+g795a4c3',
+    VERSION: '1.6.0.post124+gb568634',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
