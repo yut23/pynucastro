6b210e19f2cf007b0058c673070c1a628fea85d6
yut23 <yut23@users.noreply.github.com>
2024-06-03 18:49:33 +0000

    deploy: b796c6143303a3d73b0f513db17cd5a65eddac30

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,5 +1,5 @@
 const DOCUMENTATION_OPTIONS = {
-    VERSION: '1.6.0.post452+g2aef908',
+    VERSION: '1.6.0.post454+gb796c61',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
